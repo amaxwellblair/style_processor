b531a2f842b04cfc900fa86cbb33ded2a3d3812d
Allan Maxwell Blair <a.maxwell.blair@gmail.com>
2015-11-19 13:44:56 -0500

    completed spellcheck output

--- output.rb
+++ output.rb
@@ -10,6 +10,12 @@ class Style
 	def output_spell_check
 		mark_up = spell_check_all(@text_words)
 		wrong_words = mark_up.select{ |element| element.class == Hash}
+		corrections = []
+		wrong_words.each_with_index do |hsh,i|
+			corrections << "(#{i+1}): " + hsh[:suggests].join(", ")
+		end
+		corrections = corrections.join("\n")
+
 		text_output = @text_words
 		count = 1
 		mark_up.each_with_index do |element,i|
@@ -19,18 +25,23 @@ class Style
 				count += 1
 			end
 		end
-
-		return output_ender_tidy(text_output)
+		text_output = ender_tidy(text_output).join(" ")
+		final_text =  "\n\nSpellcheck output:\n------------------\n" +
+									text_output +
+					 				"\n"*2 + "Note: *(#) word* indicates spelling errors\nPlease"+
+									" see below for potential spelling corrections:\n" +
+									"----------------------------------------------------\n" +
+									corrections + "\n\n"
+		return final_text
 	end
 
-	def output_ender_tidy(text_arr)
+	def ender_tidy(text_arr)
 		text_arr.each_with_index do |word, i|
 			if ender?(word)
 				text_arr[i-1] = text_arr[i-1] + word
 			end
 		end
 		text_arr = delete_enders(text_arr)
-		text_arr = text_arr.join(" ")
 		return text_arr
 	end
 end
